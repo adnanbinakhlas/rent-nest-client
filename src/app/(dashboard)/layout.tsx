51ae5d627c0ec5bb058fabe14fa24bd3c15d4663
adnan-b-akhlas-dev <adnan.b.akhlas.dev@gmail.com>
2026-07-29 04:11:12 +0600

    feat: create public, auth & dashboard folder structure

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,9 @@
+import { ReactNode } from "react";
+
+interface IProps {
+  children: ReactNode;
+}
+
+export default function DashboardLayout({ children }: IProps) {
+  return <div>{children}</div>;
+}
